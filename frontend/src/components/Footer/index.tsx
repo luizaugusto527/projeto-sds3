4fe1163174cfaa45312b96662e91aaae865a5acc
unknown <luizaugustotpires@gmail.com>
2021-09-10 13:46:59 -0300

    Home, navigation

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 
-function Footer() {
+const  Footer = () => {
     return (
-        <footer className="footer mt-auto py-3 bg-dark">
+        <footer className="footer mt-auto py-3 bg-dark ">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/luizaugusto527" target="_blank" rel="noreferrer">Luiz Augusto T. Pires</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
